ddf034514a02eece3670d165c899b402c30793fe
nithin-cp <nithin.cp@hotmail.com>
2015-11-20 22:14:14 +0530

    Add new function evaluating the operands.

--- postfix.py
+++ postfix.py
@@ -5,7 +5,18 @@ def get_input():
 def push(operand,stack):
     stack.append(int(operand))
     return stack
+def eval_post(number2,number1, operator,stack):
+    if operator == '+':
+        rs = number2 + number1
+    elif operator == '*':
+        rs =  number2 * number1
+    elif operator == '/':
+        rs = number2/number1
+    elif operator == '-':
+        rs = number2 - number1
+    return rs
+            
 
 
-   
+
 
